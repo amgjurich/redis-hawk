4b46595ca76e2adbeae262df375a31f15acf6934
Abigail Gjurich <amgjurich@gmail.com>
2021-05-16 14:15:53 -0400

    committing updated webpack config and components that I just pulled down from Arthur

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-//react router
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-//import filterNav
 import FilterNav from './navbars/FilterNav.jsx';
-//import PageNav
 import PageNav from './navbars/PageNav.jsx';
-//import DatabaseNav
 import DatabaseNav from './navbars/DatabaseNav.jsx';
-//import KeyspaceComponent
 import KeyspaceComponent from './keyspace/KeyspaceComponent.jsx';
-//import GraphComponent
 import GraphComponent from './graphs/GraphComponent.jsx';
-//import EventComponent
 import EventComponent from './events/EventComponent.jsx';
 
 class App extends Component {
